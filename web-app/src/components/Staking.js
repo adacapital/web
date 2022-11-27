f6480ca257073f2cd76f8ca60fc06380bccdab22
Thomas Langlois <thomas.langlois@mac.com>
2022-11-27 14:52:50 +0000

    tweak

--- web-app/src/components/Staking.js
+++ web-app/src/components/Staking.js
@@ -151,7 +151,7 @@ const Staking = () => {
                                 <div class="col-6">
                                     <div class="card-static-item">
                                         <div class="card-static-item-title">Live Stake</div>
-                                        <div class="card-static-item-value"><strong><span>269,593<sup> ADA</sup></span></strong></div>
+                                        <div class="card-static-item-value"><strong><span>194,075<sup> ADA</sup></span></strong></div>
                                     </div>
                                 </div>
                                 <div class="col-6">
@@ -179,7 +179,7 @@ const Staking = () => {
                                 <div class="col-6">
                                     <div class="card-static-item">
                                         <div class="card-static-item-title">Saturation</div>
-                                        <div class="card-static-item-value"><strong><span>0.4%</span></strong></div>
+                                        <div class="card-static-item-value"><strong><span>0.28%</span></strong></div>
                                     </div>
                                 </div>
                                 <div class="col-6">
